bd75b6ea7ed285d29cfca654ea367f8787875e0b
pete-emperor <1976479003@qq.com>
2020-09-27 18:03:06 +0800

    cs

--- demo-config-client/src/main/java/com/pyt/DemoConfigClientApplication.java
+++ demo-config-client/src/main/java/com/pyt/DemoConfigClientApplication.java
@@ -3,11 +3,15 @@ package com.pyt;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
+@RefreshScope
 public class DemoConfigClientApplication {
 
 	public static void main(String[] args) {
@@ -16,11 +20,11 @@ public class DemoConfigClientApplication {
 
 
 
-	@Value("${spring.rabbitmq.host}")
+	@Value("${foo}")
 	String foo;
 
 
-	@Value("${spring.rabbitmq.port}")
+	@Value("${democonfigclient.message}")
 	String message;
 
 	@RequestMapping(value = "/hi")
